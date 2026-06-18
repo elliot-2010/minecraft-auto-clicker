ed613443b9bc8a6d1806c31140ebe36eeaa3d86e
elliot <kermimamad156@gmail.com>
2026-06-18 12:34:01 +0330

    Add Find Button and auto click for right button

--- ACM.py
+++ ACM.py
@@ -3,19 +3,29 @@ import threading
 import time
 
 mouse = Controller()
-clicking = False
+clicking_left = False
+clicking_right = False
 
 def click_loop():
     while True:
-        if clicking:
+        if clicking_left:
             mouse.click(Button.left)
             time.sleep(1/20)   # 20 کلیک در ثانیه
+        if clicking_right:
+            mouse.click(Button.right)
+            time.sleep(1/20)
 
 def on_click(x, y, button, pressed):
-    global clicking
+    global clicking_left
+    global clicking_right
     if button == Button.button9 and pressed:
-        clicking = not clicking   # toggle: روشن/خاموش
-        print("Auto-click:", "ON" if clicking else "OFF")
+        clicking_left = not clicking_left   # toggle: روشن/خاموش
+        print("Auto-click-left:", "ON" if clicking_left else "OFF")
+    if button == Button.button8 and  pressed:
+        clicking_right = not clicking_right
+        print("Auto-click:-right", "ON" if clicking_right else "OFF")
+
+    
 
 # Thread برای کلیک کردن
 t = threading.Thread(target=click_loop, daemon=True)
